8481028c4411970f34ad98289573663fe0581afa
Rob Rau <rfr2@mac.com>
2016-10-31 18:02:34 -0400

    Added python script to make a series of images from save files, used for making a video. Pulled out some stuff to PlotUtils.py

---
+++ scripts/python/MakeEbbVideo.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3.4
+
+import re
+import sys
+import os
+from os import listdir
+from os.path import isfile, join
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+import EbbUtils as eu
+import PlotUtils as pu
+
+def plotstate(Mesh, U, field, fname):
+	V = Mesh['V']
+	E = Mesh['E']
+	BE = Mesh['BE']
+
+	f = plt.figure(figsize=(12,6))
+
+	F = pu.getField(U, field)
+	plt.tripcolor(V[:,0], V[:,1], triangles=E, facecolors=F, shading='flat', vmin=0.0, vmax=0.8)
+
+	for i in range(len(BE)):
+		x = [V[BE[i,0],0], V[BE[i,1],0]]
+		y = [V[BE[i,0],1], V[BE[i,1],1]]
+		plt.plot(x, y, '-', linewidth=2, color='black')
+	
+	#dosave = (len(fname) != 0)
+
+	plt.axis('equal')
+
+	#plt.axis([-100, 100,-100, 100])
+	plt.axis([-2, 10,-4, 4])
+	plt.colorbar()
+	#plt.clim(0, 0.8)
+	plt.title(field, fontsize=16)
+
+	f.tight_layout()
+	plt.show()#block=(not dosave))
+	#if (dosave):
+	plt.savefig(fname)
+	
+	plt.close(f)
+
+
+def tryint(s):
+    try:
+        return int(s)
+    except:
+        return s
+
+def alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [ tryint(c) for c in re.split('([0-9]+)', s) ]
+
+def sort_nicely(l):
+    """ Sort the given list in the way that humans expect.
+    """
+    l.sort(key=alphanum_key)
+
+if __name__ == "__main__":
+	
+	if len(sys.argv) < 3:
+		print('Not enough input arguments')
+		sys.exit()
+
+	meshFile = sys.argv[1]
+	state = sys.argv[2]
+
+	mesh = eu.importEbbMatlabMesh(meshFile)
+
+	slnFiles = [f for f in listdir("./") if (isfile(join("./", f)) and ("esln" in f) and ("final" not in f))]
+	sort_nicely(slnFiles)
+	
+	os.mkdir("output")
+	for idx in range(len(slnFiles)):
+		U = eu.importEbbSolution(slnFiles[idx])
+		plotstate(mesh, U, state, "output/"+str(idx).zfill(8)+".png")
+	
+	#ffmpeg -framerate 30 -i %08d.png -c:v libx264 mach.mp4
---
+++ scripts/python/PlotUtils.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#-----------------------------------------------------------
+def getField(U, field):
+	r = U[:,0]
+	ru = U[:,1]
+	rv = U[:,2]
+	rE = U[:,3]
+
+	g = 1.4
+	s = field.lower()
+
+	if (s == 'mach'):
+		V = np.sqrt(ru**2 + rv**2)/r
+		p = (g-1.)*(rE-0.5*r*V**2)
+		c = np.sqrt(g*p/r)
+		return V/c
+	elif (s == 'pressure'):
+		V = np.sqrt(ru**2 + rv**2)/r
+		p = (g-1.)*(rE-0.5*r*V**2)
+		return p
+	elif (s == 'density'):
+		return r
+	elif (s == 'xmomentum'):
+		return ru
+	elif (s == 'ymomentum'):
+		return rv
+	elif (s == 'energy'):
+		return rE/r
+	elif (s == 'xvelocity'):
+		return ru/r
+	elif (s == 'yvelocity'):
+		return rv/r
+
+
+#-----------------------------------------------------------
+def plotstate(Mesh, U, field, fname):
+	V = Mesh['V']
+	E = Mesh['E']
+	BE = Mesh['BE']
+
+	f = plt.figure(figsize=(12,6))
+
+	F = getField(U, field)
+	plt.tripcolor(V[:,0], V[:,1], triangles=E, facecolors=F, shading='flat')
+
+	for i in range(len(BE)):
+		x = [V[BE[i,0],0], V[BE[i,1],0]]
+		y = [V[BE[i,0],1], V[BE[i,1],1]]
+		plt.plot(x, y, '-', linewidth=2, color='black')
+	
+	dosave = (len(fname) != 0)
+
+	plt.axis('equal')
+
+	plt.axis([-100, 100,-100, 100])
+	#plt.axis([-2, 10,-4, 4])
+	plt.colorbar()
+	#plt.clim(0, 10)
+	plt.title(field, fontsize=16)
+
+	f.tight_layout()
+	plt.show(block=(not dosave))
+	if (dosave):
+		plt.savefig(fname)
+	
+	plt.close(f)
